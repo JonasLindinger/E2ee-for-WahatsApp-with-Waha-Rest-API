19155635585fa684608a883f8350fcb2cf0231c6
Jonas Lindinger <lindinger.jonas.yt@gmail.com>
2025-09-10 08:42:56 +0200

    add: advanced chat list

--- lib/screens/main.dart
+++ lib/screens/main.dart
@@ -5,6 +5,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:secure_messanger_app/main.dart';
+import 'package:secure_messanger_app/screens/chat.dart';
 
 class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
@@ -40,14 +41,21 @@ class _MainScreenState extends State<MainScreen> {
       final json = jsonDecode(body);
 
       List<Chat> newChats = [];
+      bool isFirst = true; // For testing
       for (final chat in json) {
         Chat newChat = new Chat();
         newChat.id = chat["id"];
         newChat.name = chat["name"];
-        //newChat.picture = chat["picture"];
-        newChat.lastMessage = chat["lastMessage"];
+        if (chat["picture"] != null)
+          newChat.picture = chat["picture"]; // picture is a url
+        if (chat["lastMessage"] != null)
+          newChat.lastMessage = chat["lastMessage"];
+
+        if (isFirst)
+          print(chat["lastMessage"]);
 
         newChats.add(newChat);
+        isFirst = false;
       }
 
       setState(() {
@@ -62,9 +70,10 @@ class _MainScreenState extends State<MainScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.black,
       appBar: AppBar(
         foregroundColor: Colors.white,
-        backgroundColor: Colors.green,
+        backgroundColor: Colors.black,
         title: const Text(
             "Chats",
           style: TextStyle(
@@ -76,48 +85,83 @@ class _MainScreenState extends State<MainScreen> {
       ),
       body: Container(
         margin: EdgeInsets.only(top: 20),
-        child: ListView.builder(itemCount: chats.length, itemBuilder: (context, index) => Container(
-          width: 350,
-          height: 100,
-          margin: EdgeInsets.only(top: 5, bottom: 5, right: 10, left: 10),
-          child: Container (
-              child: Row(
-            children: [
-              Container(
-                // Do the Image
-                width: 80,
-                height: 80,
-              ),
-              Container(
-                height: 100,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
+        child: ListView.builder(
+          itemCount: chats.length,
+          itemBuilder: (context, index) =>
+              GestureDetector(
+              onTap: OpenChat,
+              child: Container(
+                width: 350,
+                height: 70,
+                margin: EdgeInsets.only(top: 5, bottom: 5, right: 10, left: 10),
+                child: Container (child: Row(
                   children: [
-                    Text(
-                      chats[index].name,
-                      textAlign: TextAlign.left,
-                      style: TextStyle(
-                          fontSize: 18,
-                          fontWeight: FontWeight.w600
+                    Container(
+                      // Do the Image
+                      width: 50,
+                      height: 50,
+                      child: ClipRRect(
+                          borderRadius: BorderRadius.circular(30),
+                          child: chats[index].picture != "" ?
+                          Image.network(
+                            chats[index].picture,
+                          ) :
+                          Container(
+                            color: Colors.green,
+                          )
                       ),
                     ),
-                    Text(
-                      "Letzte Nachricht",
-                      textAlign: TextAlign.left,
-                      style: TextStyle(
-                          fontSize: 14,
-                          color: Colors.grey
+                    SizedBox(
+                      width: 20,
+                    ),
+                    Expanded(
+                      child: Container(
+                        padding: EdgeInsets.only(right: 30),
+                        height: 50,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              chats[index].name,
+                              textAlign: TextAlign.left,
+                              style: TextStyle(
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.w600,
+                                  color: Colors.white
+                              ),
+                            ),
+                            SizedBox(
+                              height: 1,
+                            ),
+                            Text(
+                              chats[index].lastMessage?["body"] ?? "",
+                              textAlign: TextAlign.left,
+                              style: TextStyle(
+                                  fontSize: 14,
+                                  color: Colors.grey
+                              ),
+                              softWrap: true,
+                              maxLines: 1,
+                              overflow: TextOverflow.ellipsis,
+                            )
+                          ],
+                        ),
                       ),
-                    )
+                    ),
                   ],
-                ),
+                )
               ),
-            ],
-          )
+            )
           ),
-        )
         ),
       )
+    );
+  }
+
+  void OpenChat() {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => ChatScreen())
     );
   }
 }
@@ -126,5 +170,5 @@ class Chat {
   String id = "";
   String name = "";
   String picture = "";
-  Map<String, dynamic> lastMessage = new Map();
+  Map<String, dynamic>? lastMessage;
 }
