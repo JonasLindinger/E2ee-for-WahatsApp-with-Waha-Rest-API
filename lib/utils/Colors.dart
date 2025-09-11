bccd789a42b07d5df9392d9fc030c721d1c04b85
Jonas Lindinger <lindinger.jonas.yt@gmail.com>
2025-09-11 16:43:13 +0200

    fix: colors

--- lib/utils/Colors.dart
+++ lib/utils/Colors.dart
@@ -1,7 +1,7 @@
 
 import 'package:flutter/material.dart';
 
-const OwnMessage = Color.fromRGBO(223, 208, 184, 255);
-const OtherMessage = Color.fromRGBO(100, 137, 121, 255);
-const AppbarBackground = Color.fromRGBO(57, 62, 70, 255);
-const AppBackground = Color.fromRGBO(34, 40, 49, 255);
+const OwnMessage = Color.fromRGBO(223, 208, 184, 1.0);
+const OtherMessage = Color.fromRGBO(100, 137, 121, 1.0);
+const AppbarBackground = Color.fromRGBO(57, 62, 70, 1.0);
+const AppBackground = Color.fromRGBO(34, 40, 49, 1.0);
